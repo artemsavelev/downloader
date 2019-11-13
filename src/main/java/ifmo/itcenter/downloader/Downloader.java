b63bc4aa7bed3a092c6a0ddba8f27d503c7af09d
artemsavelev <savelev.artem.v@gmail.com>
2019-11-14 01:27:29 +0300

    add multithreading

--- src/main/java/ifmo/itcenter/downloader/Downloader.java
+++ src/main/java/ifmo/itcenter/downloader/Downloader.java
@@ -6,47 +6,138 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Semaphore;
 
-public class Downloader implements Runnable {
+public class Downloader extends Thread {
+
+    private Semaphore sem;
     private String url;
     private String fileName;
+    private long fileSize;
+    private long resultTime;
+    private boolean full = false;
 
-
-    public Downloader(String url, String fileName) {
+    public Downloader(Semaphore sem, String url, String fileName) {
+        this.sem = sem;
         this.url = url;
         this.fileName = fileName;
     }
 
+
     @Override
     public void run() {
-
         try {
-            long start = System.currentTimeMillis();
+            // если еще не скачал
+            if (!full) {
+                //Запрашиваем у семафора разрешение на выполнение
+                sem.acquire();
 
-            URL url = new URL(this.url);
+                long start = System.currentTimeMillis();
 
-            // узнаем количество байт в файле
-            HttpURLConnection urlFileSize = (HttpURLConnection) url.openConnection();
-            long fileSize = urlFileSize.getContentLengthLong();
+                URL url = new URL(this.url);
 
-            // поток для скачивания
-            ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
+                // узнаем количество байт в файле
+                HttpURLConnection urlFileSize = (HttpURLConnection) url.openConnection();
+                fileSize = urlFileSize.getContentLengthLong();
 
-            // сохраняем на диск
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-            fileOutputStream.close();
+                // поток для скачивания
+                ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
+                System.out.printf("Загружается файл: %s размер %s\n", fileName, getFileSize());
 
-            long end = System.currentTimeMillis();
-            long result = end - start;
+                // сохраняем на диск
+                FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+                fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+                fileOutputStream.close();
 
-            System.out.printf("Скачан файл: %s в файл: %s размер %s bytes за %s ms\n", this.url, fileName, fileSize, result);
+                long end = System.currentTimeMillis();
+                resultTime = end - start;
+                double speed = ((fileSize * 8) / (resultTime / 1000d)) / 1000;
 
 
+                System.out.printf("Файл: %s загружен размер %s за %s на скорости %.1f kB/s\n", fileName, getFileSize(), getResultTime(), speed);
+
+                full = true;
+                sem.release();
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("Что-то пошло не так!");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+    private String getFileSize() {
+
+        if (fileSize < 1024) {
+            return fileSize + " B";
+        }
+        int z = (63 - Long.numberOfLeadingZeros(fileSize)) / 10;
+        return String.format("%.1f %sB", (double) fileSize / (1L << (z * 10)), " KMGTPE".charAt(z));
+    }
+
+    public String getResultTime() {
+
+        double seconds = (double) resultTime / 1000;
+        int s = (int) seconds % 60;
+        int minutes = (int) seconds / 60;
+
+        if (minutes == 0) {
+            return String.format("%.3f %s", seconds, morpher((int) seconds, "s"));
+        }
+
+        return String.format("%2d %s %2d %s", minutes, morpher(minutes, "m"), s, morpher(s, "s"));
+    }
+
+    private String morpher(int count, String type) {
+        String one = "";
+        String two = "";
+        String three = "";
+
+        if (type.equals("m")) {
+
+            one = "минуту";
+            two = "минуты";
+            three = "минут";
+        }
+
+        if (type.equals("s")) {
+
+            one = "секунду";
+            two = "секунды";
+            three = "секунд";
+        }
+
+        switch (count) {
+            case 1:
+            case 21:
+            case 31:
+            case 41:
+            case 51:
+                return one;
+            case 2:
+            case 3:
+            case 4:
+            case 22:
+            case 23:
+            case 24:
+            case 32:
+            case 33:
+            case 34:
+            case 42:
+            case 43:
+            case 44:
+            case 52:
+            case 53:
+            case 54:
+                return two;
+            default:
+                return three;
+        }
+    }
+
 
 }
--- src/main/java/ifmo/itcenter/downloader/Main.java
+++ src/main/java/ifmo/itcenter/downloader/Main.java
@@ -3,45 +3,94 @@ package ifmo.itcenter.downloader;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+import java.util.concurrent.Semaphore;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static final String MESSAGE = "Программа имеет параметры коммандной строки\n\n" +
+            "  Входные параметры:\n" +
+            "  1. количество одновременно качающих потоков (1,2,3,4....)\n" +
+            "  2. имя папки, куда складывать скачанные файлы\n" +
+            "  3. путь к файлу со списком ссылок\n\n" +
+            "  Пример вызова:\n" +
+            "  java -jar utility.jar 5 output_folder links.txt\n";
 
+    public static void main(String[] args) {
 
-        int numberOfFlows = 0;
-        String nameOfDir = "";
-        String linkOfList = "/mnt/sdb/java/links.txt";
+        int numberOfFlows;
+        String nameOfDir;
+        String linkOfList;
+        Path dir = Paths.get(args[1]);
+        Path file = Paths.get(args[2]);
 
+        if (args.length != 3) {
+            System.out.println(MESSAGE);
+            return;
+        } else {
+            numberOfFlows = Integer.parseInt(args[0]);
 
-        List<Downloader> downloader = new ArrayList<>();
+            if (!Files.isDirectory(dir)) {
+                nameOfDir = dir.toString();
+            } else {
+                System.out.println("Переданный параметр " +
+                        "\"" + dir.toString() + "\" " +
+                        "не является папкой, проверте правильность ввода");
+                return;
+            }
+
+            if (Files.isRegularFile(file)) {
+                linkOfList = file.toString();
+            } else {
+                System.out.println("Переданный параметр " +
+                        "\"" + file.toString() + "\" " +
+                        "не является файлом, проверте правильность ввода");
+                return;
+            }
+        }
+
+//        numberOfFlows = 2;
+//        nameOfDir = "";
+//        linkOfList = "/mnt/sdb/java/links.txt";
+
+        Semaphore sem = new Semaphore(numberOfFlows);
+        Downloader downloader = null;
+        TaskFile taskFile = new TaskFile(linkOfList);
         List<Thread> threads = new ArrayList<>();
 
-        TaskFile taskFile = new TaskFile(linkOfList);
+        System.out.println("Получены задания на загрузку:");
 
-        System.out.println("Получены задания:");
+        for (int i = 0; i < taskFile.getListOfTasks().size(); i++) {
+            ListOfTask listOfTask = taskFile.getListOfTasks().get(i);
+            System.out.println("Из " + listOfTask.getUrl() + " в файл " + listOfTask.getFileName());
 
-
-        for (ListOfTask listOfTask : taskFile.getListOfTasks()) {
-
-            Downloader downloadTask = new Downloader(listOfTask.getUrl(), listOfTask.getFileName());
-            downloader.add(downloadTask);
-
-            Thread thread = new Thread(downloadTask);
-            threads.add(thread);
-            thread.start();
-
+            downloader = new Downloader(sem, listOfTask.getUrl(), listOfTask.getFileName());
+            downloader.start();
+            threads.add(downloader);
         }
-
 
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
-
+        printResult(taskFile.getListOfTasks().size(), 0, 0, 0);
 
     }
+
+    private static void printResult(int count, int totalSize, int totalTime, int avgSpeed) {
+        System.out.println("__________________________________________________________________________________\n");
+        System.out.println("Загружено: " + count + " файлов, " + totalSize);
+        System.out.println("Время: ");
+        System.out.println("Средняя скорость: " + avgSpeed + "\n");
+    }
+
+
 }
+
